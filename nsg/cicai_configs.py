020e0b4b84dc6781c89177ba1de861c441d0c14f
Sylvia6 <1505723831@qq.com>
2023-08-11 08:50:03 +0000

    update

--- nsg/cicai_configs.py
+++ nsg/cicai_configs.py
@@ -121,8 +121,8 @@ PLUS_Recon_NSG_Car_Depth_Semantic = MethodSpecification(
                     use_single_jitter=False,
                     semantic_loss_weight=0.1
                 ),
-                object_model_template=CarNeRFModelConfig(_target=CarNeRF),
-                object_representation="object-wise",
+                # object_model_template=CarNeRFModelConfig(_target=CarNeRF),
+                object_representation="class-wise",
                 object_ray_sample_strategy="remove-bg",
             ),
         ),
@@ -157,13 +157,13 @@ KITTI_Recon_NSG_Car_Depth = MethodSpecification(
         pipeline=NSGPipelineConfig(
             datamanager=NSGkittiDataManagerConfig(
                 dataparser=NSGkittiDataParserConfig(
-                    use_car_latents=False,
+                    use_car_latents=True,
                     use_depth=True,
-                    # car_object_latents_path=Path(
-                    #     "/data1/chenjt/datasets/ckpts/pretrain/car_nerf/latent_codes_car_van_truck.pt"
-                    # ),
+                    car_object_latents_path=Path(
+                        "/data1/chenjt/datasets/ckpts/pretrain/car_nerf/latent_codes_car_van_truck.pt"
+                    ),
                     split_setting="reconstruction",
-                    # car_nerf_state_dict_path=Path("/data1/chenjt/datasets/ckpts/pretrain/car_nerf/epoch_670.ckpt"),
+                    car_nerf_state_dict_path=Path("/data1/chenjt/datasets/ckpts/pretrain/car_nerf/epoch_670.ckpt"),
                 ),
                 train_num_rays_per_batch=4096,
                 eval_num_rays_per_batch=4096,
@@ -172,8 +172,7 @@ KITTI_Recon_NSG_Car_Depth = MethodSpecification(
             model=SceneGraphModelConfig(
                 background_model=NerfactoModelConfig(),
                 object_model_template=CarNeRFModelConfig(_target=CarNeRF),
-                object_representation="obj-wise",
-                # "class-wise",
+                object_representation="class-wise",
                 object_ray_sample_strategy="remove-bg",
             ),
         ),
